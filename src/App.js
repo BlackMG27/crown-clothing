d7a8950e8cac9612439bfb6f2cbda91fe9622a84
BlackMG27 <oike279@gmail.com>
2019-09-11 13:49:54 -0400

    updated menu-items with navigation using withRouter higher order components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/pages/homepage/homepage.component';
 
+const HatsPage = () => ( < div > < h1 > Hats Page < /h1> < /div > )
+
+const JacketPage = () => ( < div > < h1 > JacketPage < /h1> < /div > )
+
+const SneakerPage = () => ( < div > < h1 > SneakerPage < /h1> < /div > )
+
+const WomensPage = () => ( < div > < h1 > Womens Page < /h1> < /div > )
+
+const MensPage = () => ( < div > < h1 > Mens Page < /h1> < /div > )
+
 function App() {
-  return (
-    <Fragment>
-      <HomePage/>
-    </Fragment>
-  );
+    return ( < Fragment > < Switch > < Route exact path = '/'
+        component = {
+            HomePage
+        }
+        /> < Route exact path = '/hats
+        ' component = {
+        HatsPage
+    }
+    /> < Route path = '/jackets
+    ' component = {
+    JacketPage
+}
+/> < Route path = '/sneakers
+' component = {
+SneakerPage
+}
+/> < Route path = '/womens
+' component = {
+WomensPage
+}
+/> < Route path = '/mens
+' component = {
+MensPage
+}
+/> < /
+Switch > < /Fragment >);
 }
 export default App;
