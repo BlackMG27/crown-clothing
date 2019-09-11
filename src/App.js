76eb8c02096b80b496d9ced711da62333f25d091
BlackMG27 <oike279@gmail.com>
2019-09-11 17:05:07 -0400

    added a shop page created collections panel and items

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
-import React, { Fragment } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, {Fragment} from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './components/pages/homepage/homepage.component';
-
-const HatsPage = () => ( < div > < h1 > Hats Page < /h1> < /div > )
-
-const JacketPage = () => ( < div > < h1 > JacketPage < /h1> < /div > )
-
-const SneakerPage = () => ( < div > < h1 > SneakerPage < /h1> < /div > )
-
-const WomensPage = () => ( < div > < h1 > Womens Page < /h1> < /div > )
-
-const MensPage = () => ( < div > < h1 > Mens Page < /h1> < /div > )
+import ShopPage from './components/pages/shop/shop.component';
 
 function App() {
-    return ( < Fragment > < Switch > < Route exact path = '/'
-        component = {
-            HomePage
-        }
-        /> < Route exact path = '/hats
-        ' component = {
-        HatsPage
-    }
-    /> < Route path = '/jackets
-    ' component = {
-    JacketPage
-}
-/> < Route path = '/sneakers
-' component = {
-SneakerPage
-}
-/> < Route path = '/womens
-' component = {
-WomensPage
-}
-/> < Route path = '/mens
-' component = {
-MensPage
-}
-/> < /
-Switch > < /Fragment >);
+    return (
+        <Fragment>
+            <Switch>
+                <Route exact path="/" component={HomePage}/>
+                <Route path='/shop' component={ShopPage}/>
+            </Switch>
+        </Fragment>
+    );
 }
 export default App;
