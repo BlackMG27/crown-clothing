3b891e0981457eebb32be724ea249e06ad2bc43e
BlackMG27 <oike279@gmail.com>
2019-09-19 13:09:18 -0400

    added new shop_data ids and cartItem quantity with redux

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,13 +3,16 @@ import './custom-button.styles.scss';
 
 const CustomButton = ({
     children,
+    inverted,
     isGoogleSignIn,
     ...otherProps
 }) => (
     <button
-        className={`${isGoogleSignIn
-        ? 'google-sign-in'
-        : ''} custom-button`}
+        className={`${inverted
+        ? 'inverted'
+        : ''} ${isGoogleSignIn
+            ? 'google-sign-in'
+            : ''} custom-button`}
         {...otherProps}>
         {children}
     </button>
--- src/components/preview/preview-collection.component.jsx
+++ src/components/preview/preview-collection.component.jsx
@@ -6,10 +6,7 @@ const PreviewCollection = ({title, items}) => (
     <div className="collection-preview">
         <h2 className="title">{title.toUpperCase()}</h2>
         <div className="preview">
-            {items.filter((item, idx) => idx < 4).map(({
-                id,
-                ...otherItemProps
-            }) => (<CollectionItem key={id} {...otherItemProps}/>))
+            {items.filter((item, idx) => idx < 4).map(item => (<CollectionItem key={item.id} item={item}/>))
 }
         </div>
     </div>
