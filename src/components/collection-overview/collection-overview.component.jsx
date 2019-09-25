a14600da78ab77e369f859cb2dd89e6b94d30d9e
BlackMG27 <oike279@gmail.com>
2019-09-25 18:34:49 -0400

    fixed the /shop collection preview component and edited the collection-item styles

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -5,17 +5,20 @@ import PreviewCollection from '../preview/preview-collection.component';
 import {selectCollections} from '../../redux/shop/shop.selector';
 import './collection-overview.styles.scss';
 
-const CollectionOverview = ({collections}) => (
-    <div className="collections-overview">
-        {collections.map(({
-            id,
-            ...otherCollectionProps
-        }) => (<PreviewCollection key={id} {...otherCollectionProps}/>))
+const CollectionOverview = ({collections}) => {
+    const collectionsArray = Object.values(collections);
+    return (
+        <div className="collections-overview">
+            {collectionsArray.map(({
+                id,
+                ...otherCollectionProps
+            }) => (<PreviewCollection key={id} {...otherCollectionProps}/>))
 }
 
-    </div>
-)
+        </div>
+    )
+}
 
-const mapStateToProps = createStructuredSelector({collections: selectCollections})
+const mapStateToProps = createStructuredSelector({collections: selectCollections});
 
-export default connect(mapStateToProps)(CollectionOverview)
+export default connect(mapStateToProps)(CollectionOverview);
